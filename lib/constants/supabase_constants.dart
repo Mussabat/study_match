f27641f9910681a0fc62af307d20fed33fe6d4d3
Mussabat <tnafisa18@gamil.com>
2024-01-03 23:14:53 +0600

    project setup

---
+++ lib/constants/supabase_constants.dart
@@ -0,0 +1,6 @@
+class SupabaseConstants {
+  SupabaseConstants._();
+  static const String supabaseUrl = 'https://kcclyivogntgwcsahubo.supabase.co';
+  static const String supabaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjY2x5aXZvZ250Z3djc2FodWJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc4ODgyNTYsImV4cCI6MjAxMzQ2NDI1Nn0.YUvEtG7NeR8Tpm5pB3-Wa3vbbqMSDlbE-K62XCeYPag';
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:study_match/constants/supabase_constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  await Supabase.initialize(
+    url: SupabaseConstants.supabaseUrl,
+    anonKey: SupabaseConstants.supabaseKey,
+  );
+  runApp(const MyApp());
+}
+
+final supabase = Supabase.instance.client;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
